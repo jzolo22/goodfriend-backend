700777a672b1c3518f848ed4d2bb78bb8cf21769
jzolo22 <jzolotarev@gmail.com>
2021-01-14 16:36:59 -0500

    additional data seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,15 +10,24 @@ Api::V1::User.destroy_all
 Api::V1::Follow.destroy_all
 Api::V1::Event.destroy_all
 
-u1 = Api::V1::User.create!(first_name: "Julia", last_name: "Zolotarev")
-u2 = Api::V1::User.create!(first_name: "Dimitrios", last_name: "Fragiskatos")
+u1 = Api::V1::User.create!(first_name: "Julia", last_name: "Zolotarev", birthday: Date.new(1991, 9, 22), address: "1340 St Marks Ave, Brooklyn NY 11233", partner_name: "Dimitrios", partner_birthday: Date.new(1983, 9, 7), flowers: true)
+u2 = Api::V1::User.create!(first_name: "Dimitrios", last_name: "Fragiskatos", birthday: Date.new(1983, 9, 7), address: "1340 St Marks Ave, Brooklyn NY 11233", partner_name: "Julia", partner_birthday: Date.new(1991, 9, 22), flowers: false)
+u3 = Api::V1::User.create!(first_name: "John", last_name: "Jennison", birthday: Date.new(1983, 9, 7), address: "some place in Brooklyn", partner_name: "John's partner", partner_birthday: Date.new(1991, 9, 22), flowers: true)
+u4 = Api::V1::User.create!(first_name: "Sabrina", last_name: "Hutchins")
+u5 = Api::V1::User.create!(first_name: "Geoffrey", last_name: "Yang")
 
 e1 = Api::V1::Event.create!(title: "Store Anniversary", user_id: Api::V1::User.second.id, date: Date.new(2021,3,10))
 e2 = Api::V1::Event.create(title: "Bootcamp Graduation!", user_id: Api::V1::User.first.id, date: Date.new(2021, 1, 29))
-e3 = Api::V1::Event.create(title: "D's event", user_id: Api::V1::User.second.id)
+e3 = Api::V1::Event.create(title: "D's event", user_id: u2.id, date: Date.new(2021, 1, 20))
+e4 = Api::V1::Event.create(title: "John's event", user_id: u3.id, date: Date.new(2021, 1, 16))
+e5 = Api::V1::Event.create(title: "Sabrina's event", user_id: u4.id, date: Date.new(2021, 2, 8))
+e6 = Api::V1::Event.create(title: "Geoffrey's event", user_id: u5.id, date: Date.new(2021, 1, 18))
 
 
 f1 = Api::V1::Follow.create!(follower_id: Api::V1::User.second.id, followee_id: Api::V1::User.first.id)
+f2 = Api::V1::Follow.create!(follower_id: u2.id, followee_id: u3.id)
+f3 = Api::V1::Follow.create!(follower_id: u2.id, followee_id: u4.id)
+f4 = Api::V1::Follow.create!(follower_id: u2.id, followee_id: u5.id)
 
 
 # Create array of current user's follower's events:
