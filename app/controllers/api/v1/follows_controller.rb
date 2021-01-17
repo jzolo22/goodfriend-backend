a21c1aa433a5b389288bda78d56b0fd74bbeeb0b
jzolo22 <jzolotarev@gmail.com>
2021-01-17 18:43:50 -0500

    added destroy action for follows

--- app/controllers/api/v1/follows_controller.rb
+++ app/controllers/api/v1/follows_controller.rb
@@ -5,6 +5,13 @@ class Api::V1::FollowsController < ApplicationController
         render json: { follow: Api::V1::FollowSerializer.new(follow) }, status: :accepted
     end
 
+    def destroy 
+        id_array = params[:id].split("&")
+        follower_id = id_array[0]
+        followee_id = id_array[1]
+        follow = Api::V1::Follow.find_by(follower_id: follower_id, followee_id: followee_id)
+        follow.destroy 
+    end
 
     private
 
