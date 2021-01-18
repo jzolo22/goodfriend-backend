1f9568a751ff36e8094d2c3832084ed228fa5eea
jzolo22 <jzolotarev@gmail.com>
2021-01-18 16:45:48 -0500

    added create method to events controller

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -4,4 +4,14 @@ class Api::V1::EventsController < ApplicationController
         events = Api::V1::Event.all 
         render json: events
     end
+
+    def create
+        event = Api::V1::Event.create!(event_params)
+        render json: event
+    end
+
+    private
+    def event_params
+        params.require(:event).permit(:user_id, :title, :date, :type, :annual, :description, :celebrator_id)
+    end
 end
