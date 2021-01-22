de3b84f14a8c0d4342ef3482c8cc6c432a7fb0e3
jzolo22 <jzolotarev@gmail.com>
2021-01-22 14:47:30 -0500

    added create action for items and wishlists

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,2 +1,13 @@
 class Api::V1::ItemsController < ApplicationController
+
+    def create
+        item = Api::V1::Item.create(item_params)
+        render json: {item: Api::V1::ItemSerializer.new(item)}
+    end
+
+    private
+
+    def item_params
+        params.require(:item).permit(:wishlist_id, :name, :link, :purchased)
+    end
 end
--- app/controllers/api/v1/wishlists_controller.rb
+++ app/controllers/api/v1/wishlists_controller.rb
@@ -1,2 +1,13 @@
 class Api::V1::WishlistsController < ApplicationController
+
+    def create 
+        wishlist = Api::V1::Wishlist.create!(wishlist_params)
+        render json: wishlist
+    end
+
+    private
+
+    def wishlist_params
+        params.require(:wishlist).permit(:user_id)
+    end
 end
