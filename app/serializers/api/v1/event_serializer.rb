70c97839e6b9b0dc2535635784b33550ade36ff6
jzolo22 <jzolotarev@gmail.com>
2021-01-24 21:28:57 -0500

    updated event serializer taking away color information

--- app/serializers/api/v1/event_serializer.rb
+++ app/serializers/api/v1/event_serializer.rb
@@ -1,15 +1,15 @@
 class Api::V1::EventSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :title, :date, :annual, :description, :initials, :first_color, :second_color
+  attributes :id, :user_id, :title, :date, :annual, :description, :initials
 
   def initials
     "#{self.object.user.first_name[0]}#{self.object.user.last_name[0]}"
   end
 
-  def first_color
-    self.object.user.first_color
-  end
+  # def first_color
+  #   self.object.user.first_color
+  # end
 
-  def second_color
-    self.object.user.second_color
-  end
+  # def second_color
+  #   self.object.user.second_color
+  # end
 end
